029b3fb16c01aa41b54f35c2db8ea172bcc6528e
teamplay <67694293+hexter31376@users.noreply.github.com>
2025-11-10 11:05:18 +0900

    initial commit

---
+++ src/main/java/org/teamplay/notifia/NotifiaApplication.java
@@ -0,0 +1,13 @@
+package org.teamplay.notifia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotifiaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NotifiaApplication.class, args);
+    }
+
+}
